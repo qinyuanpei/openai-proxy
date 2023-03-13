a465829adc91b8372b9fc0e0c8ea7ca5460ffc18
qinyuanpei <qinyuanpei@163.com>
2023-03-13 15:05:30 +0800

    initial-commit

---
+++ server/api.py
@@ -0,0 +1,28 @@
+from fastapi import FastAPI, Header, HTTPException
+from typing import List, Optional
+from pydantic import BaseModel
+import openai
+
+class MessageModel(BaseModel):
+    role: str
+    content: str
+
+class RequestModel(BaseModel):
+    model: str
+    message: List[MessageModel]
+    max_tokens: int
+    temperature: float
+    frequency_penalty: float
+    presence_penalty: float
+
+app = FastAPI()
+
+@app.post("/openai/v1/completions")
+def do_proxy(request: RequestModel, authorization: Optional[str] = Header(None)):
+    if (authorization == None):
+        raise HTTPException(status_code=401, detail="OPENAI_API_KEY is required.")
+    
+    openai.api_key = authorization.replace("Bearer", "").strip()
+    completion = openai.ChatCompletion.create()
+    return completion
+    
