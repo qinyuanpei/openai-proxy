17f38149c754da3300a6d090f491a519f0e679c6
songli zhao <songli.zhao@qq.com>
2023-04-01 12:27:58 +0800

    fix: v1/completions

--- api.py
+++ api.py
@@ -9,35 +9,46 @@ from utils import resp_200
 
 app = FastAPI()
 
+openai.api_key = os.environ.get("OPENAI_API_KEY")  # 从环境变量中读取API_KEY
+
 @app.get("/")
 def do_echo():
     return {"message": "This is a greet from FastAPI.", "timestamp": datetime.now()}
 
 @app.post("/v1/completions")
 async def do_proxy_chat(request: dict, authorization: Optional[str] = Header(None)):
-    if (authorization == None):
-        raise HTTPException(status_code=401, detail="OPENAI_API_KEY is required.")
+    # if (authorization == None):
+    #     raise HTTPException(status_code=401, detail="OPENAI_API_KEY is required.")
     
     try:
-        openai.api_key = authorization.replace("Bearer", "").strip()
-        completion = openai.ChatCompletion.create(model=request['model'], messages=request['messages'])
-        return resp_200(data=completion)
+        # openai.api_key = authorization.replace("Bearer", "").strip()
+        # completion = openai.ChatCompletion.create(model=request['model'], messages=request['messages'])
+        """
+        调用OpenAI生成文本
+        """
+        completions = openai.Completion.create(
+            engine      = request['model'],
+            prompt      = request['prompt'],
+            max_tokens  = 1024 if request['max_tokens'] else request['max_tokens'] ,
+            temperature = 0.7 if request['temperature'] else request['temperature']
+        )
+        return resp_200(data=completions)
     except Exception as e:
         logging.error(e)
         raise HTTPException(status_code=500, detail="")
 
-@app.post("/v1/audio/transcriptions")   
-async def do_proxy_whisper(file: UploadFile, authorization: Optional[str] = Header(None)):
-    if (authorization == None):
-        raise HTTPException(status_code=401, detail="OPENAI_API_KEY is required.")
+# @app.post("/v1/audio/transcriptions")   
+# async def do_proxy_whisper(file: UploadFile, authorization: Optional[str] = Header(None)):
+#     if (authorization == None):
+#         raise HTTPException(status_code=401, detail="OPENAI_API_KEY is required.")
     
-    try:
-        openai.api_key = authorization.replace("Bearer", "").strip()
-        contents = await file.read()
-        audio = openai.Audio(BytesIO(contents))
-        transcript = openai.Audio.transcribe("whisper-1", audio)
-        return resp_200(data=transcript)
-    except Exception as e:
-        logging.error(e)
-        raise HTTPException(status_code=500, detail="")
+#     try:
+#         openai.api_key = authorization.replace("Bearer", "").strip()
+#         contents = await file.read()
+#         audio = openai.Audio(BytesIO(contents))
+#         transcript = openai.Audio.transcribe("whisper-1", audio)
+#         return resp_200(data=transcript)
+#     except Exception as e:
+#         logging.error(e)
+#         raise HTTPException(status_code=500, detail="")
     
---
+++ test.py
@@ -0,0 +1,6 @@
+curl --location --request POST 'http://100.26.242.75:8000/v1/completions' \
+-H 'Content-Type: application/json' \
+-d '{
+"model": "gpt-3.5-turbo",
+"messages": [{"role": "user", "content":"Hello ChatGPT!"}]
+}'
