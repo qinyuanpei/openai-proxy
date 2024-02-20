84d0e05b98972e0418c05a2f2d8a5201aed21f5c
qinyuanpei <qinyuanpei@shinetechsoftware.com>
2024-02-20 11:29:09 +0800

    use vercel.json to proxy openai

--- utils.py
+++
@@ -1,9 +0,0 @@
-from fastapi import status
-from fastapi.responses import JSONResponse, Response  
-from typing import Union
-
-def resp_200(*, data: Union[list, dict, str]) -> Response:
-    return JSONResponse(
-        status_code=status.HTTP_200_OK,
-        content=data
-    ) 
